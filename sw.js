c72a8869ede41982dc3f7fa8a01ea33c2c304284
B0BERT1US <Kelly-D104@ulster.ac.uk>
2026-08-28 21:32:37 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+/* Bump this name every time you push a new version */
+const CACHE = 'workshop-v1';
+const FILES = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+/* Network first so updates show up straight away, cache when offline */
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET' || !e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }).catch(() =>
+      caches.match(e.request, { ignoreSearch: true }).then(r => r || caches.match('./index.html'))
+    )
+  );
+});
